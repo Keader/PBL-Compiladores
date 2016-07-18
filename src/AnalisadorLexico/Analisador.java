23158564448652d6971f765ada12daf214cbecf1
Keader <keader.android@gmail.com>
2016-07-18 20:25:12 -0300

    Apenas para teste

--- src/AnalisadorLexico/Analisador.java
+++ src/AnalisadorLexico/Analisador.java
@@ -7,7 +7,7 @@ public class Analisador {
     
     public static void main(String args[]){
         //Classe usada pra teste e.e
-        Jarvis jarvis = new Jarvis();
-        jarvis.Executar();
+        //Jarvis jarvis = new Jarvis();
+        //jarvis.Executar();
     }
 }
