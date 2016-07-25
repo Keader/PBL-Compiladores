49320084b84c57c9e1f830333b124afe9e66fa5a
Keader <keader.android@gmail.com>
2016-07-25 19:35:24 -0300

    Implementando a "coluna" no Token.

--- src/Util/Token.java
+++ src/Util/Token.java
@@ -4,45 +4,49 @@ import Util.Jarvis.PadraoRegex;
 
 public class Token {
 
-	private int id;
-	private int nLinha;
+    private int id;
+    private int nLinha;
+    private int nColuna;
 
-	private String lexema;
-	private String classe;
+    private String lexema;
+    private String classe;
 
-	public Token(int id, String lexema, int nLinha) {
-		this.id = id;
-		this.lexema = lexema;
-		this.nLinha = nLinha;
-	}
+    public Token(int id, String lexema, int nLinha, int nColuna) {
+        this.id = id;
+        this.lexema = lexema;
+        this.nLinha = nLinha;
+        this.nColuna = nColuna;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getLexema() {
-		return lexema;
-	}
+    public String getLexema() {
+        return lexema;
+    }
 
-	public String getClasse() {
-		if(classe == null)
-			classe = PadraoRegex.values()[id].name();
-		return classe;
-	}
+    public String getClasse() {
+        if (classe == null) {
+            classe = PadraoRegex.values()[id].name();
+        }
+        return classe;
+    }
 
-	@Override
-	public String toString() {
-		return "Lexema: " + lexema + " Tipo: " + Jarvis.PadraoRegex.values()[id].name() + " Criado na linha: " + nLinha;
-	}
+    @Override
+    public String toString() {
+        return "- " + lexema + " - " + Jarvis.PadraoRegex.values()[id].name() + " :" + nLinha + ":" +nColuna;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if(obj instanceof Token) {
-			Token aux = (Token) obj;
-			if(aux.getId() == this.id && aux.getLexema().equals(this.lexema))
-				return true;
-		}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Token) {
+            Token aux = (Token) obj;
+            if (aux.getId() == this.id && aux.getLexema().equals(this.lexema)) {
+                return true;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
