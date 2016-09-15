2abcbb7f1ba9d4dac91e20336fa2e8243382f749
Keader <keader.android@gmail.com>
2016-09-14 21:16:24 -0300

    Mudando para os Ids unicos dos Tokens comecar com 0

--- src/Util/Dicionario.java
+++ src/Util/Dicionario.java
@@ -5,56 +5,56 @@ package Util;
  */
 public interface Dicionario {
     //Palavras Reservadas
-    public static final int TK_PROGRAMA             =  1;
-    public static final int TK_CONST                =  2;
-    public static final int TK_VAR                  =  3;
-    public static final int TK_FUNCAO               =  4;
-    public static final int TK_INICIO               =  5;
-    public static final int TK_FIM                  =  6;
-    public static final int TK_SE                   =  7;
-    public static final int TK_ENTAO                =  8;
-    public static final int TK_SENAO                =  9;
-    public static final int TK_ENQUANTO             = 10;
-    public static final int TK_FACA                 = 11;
-    public static final int TK_LEIA                 = 12;
-    public static final int TK_ESCREVA              = 13;
-    public static final int TK_INTEIRO              = 14;
-    public static final int TK_REAL                 = 15;
-    public static final int TK_BOOLEANO             = 16;
-    public static final int TK_VERDADEIRO           = 17;
-    public static final int TK_FALSO                = 18;
-    public static final int TK_CADEIA               = 19;
-    public static final int TK_CARACTERE            = 20;
+    public static final int TK_PROGRAMA             =  0;
+    public static final int TK_CONST                =  1;
+    public static final int TK_VAR                  =  2;
+    public static final int TK_FUNCAO               =  3;
+    public static final int TK_INICIO               =  4;
+    public static final int TK_FIM                  =  5;
+    public static final int TK_SE                   =  6;
+    public static final int TK_ENTAO                =  7;
+    public static final int TK_SENAO                =  8;
+    public static final int TK_ENQUANTO             = 9;
+    public static final int TK_FACA                 = 10;
+    public static final int TK_LEIA                 = 11;
+    public static final int TK_ESCREVA              = 12;
+    public static final int TK_INTEIRO              = 13;
+    public static final int TK_REAL                 = 14;
+    public static final int TK_BOOLEANO             = 15;
+    public static final int TK_VERDADEIRO           = 16;
+    public static final int TK_FALSO                = 17;
+    public static final int TK_CADEIA               = 18;
+    public static final int TK_CARACTERE            = 19;
     //Operadores
-    public static final int TK_NAO                  = 21;
-    public static final int TK_E                    = 22;
-    public static final int TK_OU                   = 23;
-    public static final int TK_SOMA                 = 24;
-    public static final int TK_SUBTRACAO            = 25;
-    public static final int TK_MULTIPLICACAO        = 26;
-    public static final int TK_DIVISAO              = 27;
-    public static final int TK_DIFERENTE            = 28;
-    public static final int TK_IGUAL                = 29;
-    public static final int TK_MENOR                = 30;
-    public static final int TK_MENORIGUAL           = 31;
-    public static final int TK_MAIOR                = 32;
-    public static final int TK_MAIORIGUAL           = 33;
+    public static final int TK_NAO                  = 20;
+    public static final int TK_E                    = 21;
+    public static final int TK_OU                   = 22;
+    public static final int TK_SOMA                 = 23;
+    public static final int TK_SUBTRACAO            = 24;
+    public static final int TK_MULTIPLICACAO        = 25;
+    public static final int TK_DIVISAO              = 26;
+    public static final int TK_DIFERENTE            = 27;
+    public static final int TK_IGUAL                = 28;
+    public static final int TK_MENOR                = 29;
+    public static final int TK_MENORIGUAL           = 30;
+    public static final int TK_MAIOR                = 31;
+    public static final int TK_MAIORIGUAL           = 32;
     //Identificador
-    public static final int TK_ID                   = 34;
+    public static final int TK_ID                   = 33;
     //Numero
-    public static final int TK_NUMERO               = 35;
+    public static final int TK_NUMERO               = 34;
     //Delimitadores
-    public static final int TK_PONTOVIRGULA         = 36;
-    public static final int TK_VIRGULA              = 37;
-    public static final int TK_PARENTESE_A          = 38;
-    public static final int TK_PARENTESE_F          = 39;
+    public static final int TK_PONTOVIRGULA         = 35;
+    public static final int TK_VIRGULA              = 36;
+    public static final int TK_PARENTESE_A          = 37;
+    public static final int TK_PARENTESE_F          = 38;
     //Cadeia de Caracteres
-    public static final int TK_CADEIA_DE_CARACTERES = 40;
+    public static final int TK_CADEIA_DE_CARACTERES = 39;
     //Caractere Literal
-    public static final int TK_CARACTERE_L          = 41;
+    public static final int TK_CARACTERE_L          = 40;
 	//EOF ($)
-	public static final int TK_OEF                  = 42;
-	public static final int TK_EPSILON              = 43;
+	public static final int TK_OEF                  = 41;
+	public static final int TK_EPSILON              = 42;
 
     //Espaco Livre do 43-99 para possiveis modificacoes nos items acima.
     //**************************************************************************
