c77e768c551ad663b4542d1befde6ce3005866e3
Keader <keader.android@gmail.com>
2016-10-03 20:54:48 -0300

    Adicionando escopo, dimensoes (para casos de vetores/matrizes) e um booleano para se for funções na classe Simbolos

--- src/Util/Dicionario.java
+++ src/Util/Dicionario.java
@@ -209,7 +209,11 @@ public interface Dicionario {
     public static final int R_TIPO_C3                            = 223;
     public static final int R_TIPO_C4                            = 224;
     public static final int R_TIPO_C5                            = 225;
-    public static final int VOLTA_PRO_PAI                           = 226;
+    public static final int VOLTA_PRO_PAI                        = 226;
+
+    //Semantico
+
+     public static final int ESCOPO_GLOBAL                       = 0;
 
     //Enums
     public enum PadraoRegex {
--- src/Util/Simbolo.java
+++ src/Util/Simbolo.java
@@ -1,14 +1,32 @@
 package Util;
 
-public class Simbolo {
+import static Util.Dicionario.conversorIdString;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Simbolo implements Dicionario{
 	private String id;
 	private int tipo;
 	private String valor;
-	
+    private int escopo;
+    private List<Integer> dimensoes;
+    private boolean ehFuncao;
+
+
 	public Simbolo(String id, int tipo, String valor){
 		this.id = id;
 		this.tipo = tipo;
 		this.valor = valor;
+        escopo = ESCOPO_GLOBAL;
+        dimensoes = new ArrayList<>();
+	}
+
+    public Simbolo(String id, int tipo, String valor, int escopo){
+		this.id = id;
+		this.tipo = tipo;
+		this.valor = valor;
+        this.escopo = escopo;
+        dimensoes = new ArrayList<>();
 	}
 
 	public int getTipo() {
@@ -22,7 +40,7 @@ public class Simbolo {
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setTipo(int tipo) {
 		this.tipo = tipo;
 	}
@@ -30,12 +48,29 @@ public class Simbolo {
 	public void setValor(String valor) {
 		this.valor = valor;
 	}
-	
+
 	public void setId(String id) {
 		this.id= id;
 	}
-	
+
+    public int getEscopo() {
+        return escopo;
+    }
+
+    public List<Integer> getDimensoes() {
+        return dimensoes;
+    }
+
+    public boolean ehFuncao() {
+        return ehFuncao;
+    }
+
+    public void setEhFuncao(boolean ehFuncao) {
+        this.ehFuncao = ehFuncao;
+    }
+
 	public String toString(){
-		return "["+ Dicionario.conversorIdString(tipo) +"] " + id + " = " + valor;
+		return "["+ conversorIdString(tipo) +"] " + id + " = " + valor;
 	}
+
 }
