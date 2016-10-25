bfa4d3e4b1bb3b5af6ea617b370f059a65f17df0
Keader <keader.android@gmail.com>
2016-10-25 10:13:05 -0300

    Corrigido erro de logica na hora de verificar funcoes dentro de index do array

--- src/AnalisadorSemantico/AnalisadorSemantico.java
+++ src/AnalisadorSemantico/AnalisadorSemantico.java
@@ -750,7 +750,7 @@ public class AnalisadorSemantico implements Dicionario{
                         erros.add(new ErroSemantico(variavel.getId(), TIPOS_INCOMPATIVEIS, t.getnLinha()));
                     else if (variavel != null && variavel.getTipo() != TK_INTEIRO)
                         erros.add(new ErroSemantico(variavel.getId(), TIPOS_INCOMPATIVEIS, t.getnLinha()));
-                    else
+                    else if (variavel == null)
                         erros.add(new ErroSemantico(id, TIPOS_INCOMPATIVEIS, t.getnLinha()));
 
                 }
